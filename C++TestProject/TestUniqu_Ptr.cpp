2fc2e98b5ebfe72021dfe6b161cdb2910d69e560
gypsy <gypsy.gipsy.1018@gmail.com>
2018-04-11 22:50:49 +0900

    constexpr 試行

--- C++TestProject/TestUniqu_Ptr.cpp
+++ C++TestProject/TestUniqu_Ptr.cpp
@@ -5,19 +5,20 @@ using namespace std;
 
 class Person {
 private:
-	string name;
-	unique_ptr<Person> pParent;
+	string name_;
+	int age_;
+	unique_ptr<Person> pParent_;
 public:
-	explicit Person(const string name) : name(name), pParent(nullptr){}
-	explicit Person(const string name, unique_ptr<Person>&& parent) : name(name), pParent(std::move(parent)){}
+	explicit Person(const string name, int age) : name_(name), age_(age), pParent_(nullptr){}
+	explicit Person(const string name, int age, unique_ptr<Person>&& parent) : name_(name), age_(age), pParent_(std::move(parent)){}
 	~Person() {
-		cout << name << "‚Í‰ð‘Ì‚³‚ê‚½\n";
+		cout << name_ << ":" << age_ <<  "‚Í‰ð‘Ì‚³‚ê‚½\n";
 	}
 
 	inline string getParentName() {
 
-		if (pParent != nullptr) {
-			return pParent->name;
+		if (pParent_ != nullptr) {
+			return pParent_->name_;
 		} else {
 			return "";
 		}
@@ -27,12 +28,14 @@ public:
 int main() {
 
 	const string TARO = "TARO";
+	constexpr int TARO_AGE = 33;
+	constexpr int SHINTARO_AGE = 6;
 
-	unique_ptr<Person> a1 = make_unique<Person>(TARO);
+	unique_ptr<Person> a1 = make_unique<Person>(TARO,TARO_AGE);
 
 	cout << a1->getParentName() << endl;
 
-	unique_ptr<Person> child = make_unique<Person>("Shintaro", std::move(a1));
+	unique_ptr<Person> child = make_unique<Person>("Shintaro", SHINTARO_AGE, std::move(a1));
 
 	cout << child->getParentName() << endl;
 
